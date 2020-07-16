46f56f39a4c0af3ebfa9460f40fb43e8e80b12ce
pnkjsajwan <pankz.sajwan@outlook.com>
2020-07-16 15:23:37 +0530

    almost done

--- app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -2,10 +2,15 @@ package com.example.shoppinglist.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.R
 import com.example.shoppinglist.data.db.ShoppingDatabase
+import com.example.shoppinglist.data.db.entites.ShoppingItem
 import com.example.shoppinglist.data.repositories.ShoppingRepository
+import com.example.shoppinglist.other.ShoppingItemsAdapter
+import kotlinx.android.synthetic.main.activity_shopping.*
 
 class ShoppingActivity : AppCompatActivity() {
 
@@ -16,6 +21,26 @@ class ShoppingActivity : AppCompatActivity() {
         val database = ShoppingDatabase(this)
         val repository = ShoppingRepository(database)
         val factory = ShoppingViewModelFactory(repository)
+
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
+
+        val adapter = ShoppingItemsAdapter(listOf(), viewModel)
+
+        rv_shoppingitems.layoutManager = LinearLayoutManager(this)
+        rv_shoppingitems.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+        fab.setOnClickListener {
+            AddShoppingItemDialog(this, object : AddDialogListener{
+                override fun onAddButtonClicked(item: ShoppingItem) {
+                    viewModel.upsert(item)
+                }
+
+            }).show()
+        }
     }
 }
