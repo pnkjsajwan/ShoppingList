fa0ea26f0222807efacf8eac3bd98071b274a36e
pnkjsajwan <pankz.sajwan@outlook.com>
2020-07-16 14:46:25 +0530

    working in adapter

--- app/src/main/java/com/example/shoppinglist/ui/shoppinglist/AddShoppingItemDialog.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppinglist/AddShoppingItemDialog.kt
@@ -1,8 +1,29 @@
 package com.example.shoppinglist.ui.shoppinglist
 
 import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDialog
+import com.example.shoppinglist.data.db.entites.ShoppingItem
+import kotlinx.android.synthetic.main.dialog_shopping_items.*
 
-class AddShoppingItemDialog(context: Context):AppCompatDialog(context) {
+class AddShoppingItemDialog(context: Context, var addDialogListener: AddDialogListener) :
+    AppCompatDialog(context) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tvAdd.setOnClickListener {
+            val name = etName.text.toString()
+            val amount = etAmount.text.toString()
 
+            if (name.isEmpty() || amount.isEmpty()) {
+                Toast.makeText(context, "Please enter all the information", Toast.LENGTH_LONG)
+                    .show()
+                return@setOnClickListener
+            }
+            val item = ShoppingItem(name, amount.toInt())
+            addDialogListener.onAddButtonClicked(item)
+            dismiss()
+        }
+        tvCancel.setOnClickListener { cancel() }
+    }
 }
